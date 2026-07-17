7ab723c1c07936bd5bfe69a0410684197a33b3ed
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-07-17 13:40:21 +0530

    created side components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    // TODO: add github and linked-in icons with links
+    <footer className="relative z-10 border-t border-white/7 py-12 mx-auto px-6 flex flex-wrap items-center justify-center text-stone-400">
+      Made with ❤️ by vbv18
+    </footer>
+  );
+}
